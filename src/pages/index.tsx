0c0e0102515284fa4b53cc6876bd7b7f78ae0c98
= <=>
2023-01-24 16:35:30 -0300

    lazy

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,16 +4,17 @@ import Link from "next/link";
 import React, { useEffect, useState } from "react";
 import styles from "@styles/Home.module.css";
 import AvoInfo from "@src/components/AvoInfo";
+import Image from "next/image";
 
 export const getServerSideProps = async () => {
-	const response = await fetch('https://avocado-beige.vercel.app/api/avo')
-  const { data: productList }: TAPIAvoResponse = await response.json()
+  const response = await fetch("https://avocado-beige.vercel.app/api/avo");
+  const { data: productList }: TAPIAvoResponse = await response.json();
   return {
     props: {
       productList,
     },
-  }
-}
+  };
+};
 
 export default function Index({ productList }: { productList: TProduct[] }) {
   return (
@@ -32,11 +33,12 @@ export default function Index({ productList }: { productList: TProduct[] }) {
               key={product.id}
             >
               <div className={styles["product-item"]}>
-                <CardMedia
-                  component="img"
+                <Image
+                  loading="lazy"
                   alt={product.name}
                   height="200"
-                  image={product.image}
+                  width={"200"}
+                  src={product.image}
                 />
                 <div className={styles.info}>
                   <Typography gutterBottom variant="h5" component="div">
